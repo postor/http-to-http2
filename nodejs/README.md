9b41608b930884bd639c67af51d17187ed2ac68b
josh <postor@gmail.com>
2018-02-27 18:00:49 +0800

    test

--- nodejs/README.md
+++ nodejs/README.md
@@ -1,6 +1,6 @@
 # with node.js on ubuntu
 
-prepare
+## prepare
 
 ```
 # registry
@@ -26,7 +26,42 @@ git clone https://github.com/postor/http-to-http2.git
 cd http-to-http2/nodejs
 npm install
 
-#first run
+```
+
+## generate certification
+
+```
+certbot certonly
+
+#How would you like to authenticate with the ACME CA?
+#-------------------------------------------------------------------------------
+#1: Spin up a temporary webserver (standalone)
+#2: Place files in webroot directory (webroot)
+#-------------------------------------------------------------------------------
+#Select the appropriate number [1-2] then [enter] (press 'c' to cancel): 1
+
+#Enter email address (used for urgent renewal and security notices) (Enter 'c' to
+#cancel): postor@gmail.com
+#Please read the Terms of Service at
+#https://letsencrypt.org/documents/LE-SA-v1.2-November-15-2017.pdf. You must
+#agree in order to register with the ACME server at
+#https://acme-v01.api.letsencrypt.org/directory
+#-------------------------------------------------------------------------------
+#(A)gree/(C)ancel: A
+
+#Would you be willing to share your email address with the Electronic Frontier
+#Foundation, a founding partner of the Let's Encrypt project and the non-profit
+#organization that develops Certbot? We'd like to send you email about EFF and
+#our work to encrypt the web, protect its users and defend digital rights.
+#-------------------------------------------------------------------------------
+#(Y)es/(N)o: Y
+
+#Please enter in your domain name(s) (comma and/or space separated)  (Enter 'c'
+#to cancel): test.i18ntech.com
+
+
+
+
 
 
 ```
--- nodejs/app.js
+++ nodejs/app.js
@@ -1,55 +1,29 @@
-'use strict';
+const express = require('express')
+const spdy = require('spdy')
+const fs = require('fs')
+const redirectToHTTPS = require('express-http-to-https')
 
-// returns an instance of node-greenlock with additional helper methods
-var lex = require('greenlock-express').create({
-  // set to https://acme-v01.api.letsencrypt.org/directory in production
-  server: process.env.NODE_ENV != 'production' ? 'staging' : 'https://acme-v01.api.letsencrypt.org/directory'
+const server = express()
+const certPath = '/etc/letsencrypt/live/test.i18ntech.com/'
 
-  // If you wish to replace the default plugins, you may do so here
-  //
-  , challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) }
-  , store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' })
+server.use(redirectToHTTPS())
 
-  // You probably wouldn't need to replace the default sni handler
-  // See https://git.coolaj86.com/coolaj86/le-sni-auto if you think you do
-  //, sni: require('le-sni-auto').create({})
+server.use('/', (req, res) => {
+  res.send('http2!')
+})
 
-  , approveDomains: approveDomains
-});
-function approveDomains(opts, certs, cb) {
-  // This is where you check your database and associated
-  // email addresses with domains and agreements and such
+//http
+server.listen(80, (err) => {
+  if (err) throw err
+  console.log('> Ready http on http://localhost')
+})
 
-
-  // The domains being approved for the first time are listed in opts.domains
-  // Certs being renewed are listed in certs.altnames
-  if (certs) {
-    opts.domains = certs.altnames;
-  }
-  else {
-    opts.email = 'postor@gmail.com';
-    opts.agreeTos = true;
-  }
-
-  // NOTE: you can also change other options such as `challengeType` and `challenge`
-  // opts.challengeType = 'http-01';
-  // opts.challenge = require('le-challenge-fs').create({});
-
-  cb(null, { options: opts, certs: certs });
-}
-// handles acme-challenge and redirects to https
-require('http').createServer(lex.middleware(require('redirect-https')())).listen(80, function () {
-  console.log("Listening for ACME http-01 challenges on", this.address());
-});
-
-
-
-var app = require('express')();
-app.use('/', function (req, res) {
-  res.end('Hello, World!');
-});
-
-// handles your app
-require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(443, function () {
-  console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
-});
+//http2
+spdy.createServer({
+  key: fs.readFileSync(__dirname + '/privkey.pem'),
+  cert: fs.readFileSync(__dirname + '/fullchain.pem')
+}, server)
+  .listen(433, (err) => {
+    if (err) throw err
+    console.log('> Ready http2 on https://localhost')
+  })
